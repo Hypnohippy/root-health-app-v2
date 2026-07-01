d4bb68e4f5756d0d250b7a2d9a90a32b4a11dcce
Hypnohippy <131181867+Hypnohippy@users.noreply.github.com>
2026-07-01 05:42:42 +0100

    update useEffect

--- app/playbook/page.js
+++ app/playbook/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { supabase } from "../../lib/supabase";
 import Nav from "../../components/Nav";
 import RootEnso from "../../components/RootEnso";
